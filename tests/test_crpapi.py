de697486acbf8ff88b0770f247d3be01a3e1a5c1
Danielsen <nathan.danielsen@gmail.com>
2018-02-14 10:58:42 -0500

    fixed one test

--- tests/test_crpapi.py
+++ tests/test_crpapi.py
@@ -1,5 +1,12 @@
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
 from opensecrets import CRP
 from opensecrets.exceptions import CRPError
+import os
 
 import pytest
 
@@ -7,5 +14,7 @@ def test_sanity():
     assert 1 + 1 == 2
 
 def test_api_requires_key():
-    with pytest.raises(CRPError):
-        CRP()
+    # @mock.patch.dict(os.environ,{'DATABASE_URL':'mytemp'})
+    with patch.dict(os.environ, {'OPENSECRETS_API_KEY': ''}):
+        with pytest.raises(CRPError):
+            CRP()
